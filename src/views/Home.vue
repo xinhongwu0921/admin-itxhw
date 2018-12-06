df049d2d520f5ac01b40883f53f790943ef21b7c
辛洪伍 <1872707806@qq.com>
2018-12-06 11:25:42 +0800

    首页整体布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-      首页
-    </div>
+  <div class="home">
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+  </div>
 </template>
 
 <script>
@@ -10,6 +16,49 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .home {
+    height: 100%;
+    background-color: #E5E5E5;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #009688;
+  }
+  .logo {
+    height:60px;
+    background: url(../assets/logo.png);
+    background-size: cover;
+    background-color: white;
+  }
+  .toggle-btn {
+    padding: 0 10px;
+    margin-left: -20px;
+    font-size: 36px;
+    line-height: 60px;
+    color: white;
+    cursor: pointer;
+  &:hover {
+     background-color: #00635a;
+   }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome{
+    color: white;
+  }
+  }
 </style>
